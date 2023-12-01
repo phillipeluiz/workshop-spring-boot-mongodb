abae721e118b2de2897d1050a2a518979aeac7b1
Phillipe <phillipeluiz@outlook.com>
2023-12-01 08:51:24 -0300

    Conectando com mongodb repository e service

---
+++ src/main/java/com/projectph/workshopmongo/services/UserService.java
@@ -0,0 +1,21 @@
+package com.projectph.workshopmongo.services;
+
+
+import com.projectph.workshopmongo.domain.User;
+import com.projectph.workshopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repo;
+
+    public List<User> findAll(){
+        return repo.findAll();
+    }
+
+}
